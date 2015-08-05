1be81ea664357a4183c1a925669e9eaf29474285
David <David@DavidPC>
2015-08-05 15:38:12 -0400

    Comments added

--- src/lab10sw/AssignmentSix.java
+++ src/lab10sw/AssignmentSix.java
@@ -12,76 +12,75 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 import javafx.geometry.Point2D;
+
 /**
  *
  * @author David
  */
 public class AssignmentSix extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) {
+        //intitial set up
         Pane pane = new Pane();
         Circle circle1 = new Circle(10);
         circle1.setCenterX(40);
         circle1.setCenterY(40);
         circle1.setFill(Color.BLUE);
-        
+
         Circle circle2 = new Circle(10);
         circle2.setCenterX(120);
         circle2.setCenterY(150);
         circle2.setFill(Color.BROWN);
-        
-        String distance = new Point2D(circle1.getCenterX(), circle1.getCenterY()).distance(circle2.getCenterX(), 
-                circle2.getCenterY()) + "";
-        
-        
+
+        String distance = new Point2D(circle1.getCenterX(), circle1.getCenterY()).distance(circle2.getCenterX(),
+                circle2.getCenterY()) + ""; //created distance to help declutter
+
         Line line = new Line(circle2.getCenterX(), circle2.getCenterY(), circle1.getCenterX(), circle1.getCenterY());
-        
-        Text text = new Text((line.getEndX() + line.getStartX())/2, (line.getStartY() + 
-                line.getEndY())/2, distance);
-        
+
+        Text text = new Text((line.getEndX() + line.getStartX()) / 2, (line.getStartY()
+                + line.getEndY()) / 2, distance);
+
         circle1.setOnMouseDragged(e -> {
+            //things to do when the circle is dragged
             circle1.setCenterX(e.getX());
             circle1.setCenterY(e.getY());
             line.setEndY(circle1.getCenterY());
             line.setEndX(circle1.getCenterX());
-            text.setX((line.getEndX() + line.getStartX())/2);
-            text.setY((line.getStartY() + line.getEndY())/2);
-            text.setText(new Point2D(circle1.getCenterX(), circle1.getCenterY()).distance(circle2.getCenterX(), 
-                circle2.getCenterY()) + "");
-            
-            
+            text.setX((line.getEndX() + line.getStartX()) / 2);
+            text.setY((line.getStartY() + line.getEndY()) / 2);
+            text.setText(new Point2D(circle1.getCenterX(), circle1.getCenterY()).distance(circle2.getCenterX(),
+                    circle2.getCenterY()) + "");
+
         });
-        
-        
-        
+
         circle2.setOnMouseDragged(e -> {
-            
-            
-            
+            //things to do when the cirlce is dragged
             circle2.setCenterX(e.getX());
             circle2.setCenterY(e.getY());
             line.setStartX(circle2.getCenterX());
             line.setStartY(circle2.getCenterY());
-            text.setX((line.getEndX() + line.getStartX())/2);
-            text.setY((line.getStartY() + line.getEndY())/2);
-            text.setText(new Point2D(circle1.getCenterX(), circle1.getCenterY()).distance(circle2.getCenterX(), 
-                circle2.getCenterY()) + "");
-        
+            text.setX((line.getEndX() + line.getStartX()) / 2);
+            text.setY((line.getStartY() + line.getEndY()) / 2);
+            text.setText(new Point2D(circle1.getCenterX(), circle1.getCenterY()).distance(circle2.getCenterX(),
+                    circle2.getCenterY()) + "");
+
         });
-        
-        
-        
-        
+
+        //setting the scene up
         pane.getChildren().addAll(circle1, circle2, line, text);
         Scene scene = new Scene(pane, 600, 600);
-        
+
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
+    }//start
 
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
-    
-}
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        launch(args);
+        
+    }//main
+}//class
